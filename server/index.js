048b86e1f71c4fe518d6a65571ed820a90e6baf5
Adwaith Nair <adwaith1310@gmail.com>
2023-10-26 22:14:04 +0530

    mongodb connection changes

--- server/index.js
+++ server/index.js
@@ -16,17 +16,26 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//connect to mongodb
+mongoose.connect(process.env.MONGODB_URL,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+}).then(()=>{
+  console.log('MongoDB connected')
+}).catch((err)=>{
+  console.log(err)
+})
+
+//routes
 app.get("/", (req, res) => {
   res.send("ok");
 });
-
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "success", service: "job listing platform",timestamp:new Date()});
 });
-
-//routes
 app.use("/",authRoutes)
 app.use("/",jobRoutes)
+
 
 //middleware to handle errors
 app.use((req,res,next)=>{
@@ -44,16 +53,7 @@ app.use((error,req,res,next)=>{
   })
 })
 
-app.listen(process.env.PORT, () => {
-  mongoose
-    .connect(process.env.MONGODB_URL,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log(`Server up and running on http://localhost:${process.env.PORT}`);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+//start server
+app.listen(process.env.PORT,()=>{
+  console.log(`Server running on port ${process.env.PORT}`)
+})
